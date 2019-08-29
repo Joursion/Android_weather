b2f135c45f561e9d23af39e18e32bfee03396580
yingshanglong <joursion@126.com>
2019-08-29 14:09:07 +0800

    add service

--- app/src/main/java/com/joursion/weather/WeatherActivity.java
+++ app/src/main/java/com/joursion/weather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.joursion.weather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.joursion.weather.gson.Forecast;
 import com.joursion.weather.gson.Weather;
+import com.joursion.weather.service.AutoUpdateService;
 import com.joursion.weather.util.HttpUtil;
 import com.joursion.weather.util.Utility;
 
@@ -133,6 +135,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather", responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         } else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                         }
